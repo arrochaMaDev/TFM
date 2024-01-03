ea806092ab2a4e4b153fa2695cbdfec0206b9761
arrochaMaDev <arrocha.ma.dev@gmail.com>
2024-01-03 20:09:28 +0000

    añadida logica para obtener todas las matriculas según el id del estudiante

--- app-gestion-server/src/Controllers/Matricula/Get/getMatricula.controller.ts
+++ app-gestion-server/src/Controllers/Matricula/Get/getMatricula.controller.ts
@@ -3,11 +3,17 @@ import { Response } from 'express';
 import { GetMatriculaService } from './getMatricula.service';
 import { MatriculaDb } from 'src/Modelos/Matricula/matriculaDb';
 import { GetMatriculaDto } from './getMatriculaDto';
+import { GetStudentService } from 'src/Controllers/Student/Get/getStudent.service';
+import { StudentDb } from 'src/Modelos/Student/studentDb';
 
 @Controller('matricula')
 export class GetMatriculaController {
-  constructor(private readonly getMatriculaService: GetMatriculaService) {}
+  constructor(
+    private readonly getMatriculaService: GetMatriculaService,
+    private readonly getStudentService: GetStudentService,
+  ) {}
 
+  // OBTENER MATRICULA POR ID DE LA MATRICULA
   @Get(':id')
   async getMatricula(@Param('id') id: number, @Res() response: Response) {
     try {
@@ -18,18 +24,13 @@ export class GetMatriculaController {
           .status(404)
           .json({ message: 'Matrícula no encontrada' });
       }
-
-      const matriculaDto: GetMatriculaDto = {
+      // OPCION DE CONTROLAR LOS DATOS MEDIANTE UN DTO
+      const matriculaDto = {
         id: matricula.id,
         student: {
           id: matricula.student.id,
-          usuario_id: matricula.student.usuario_id,
           nombre: matricula.student.nombre,
           apellidos: matricula.student.apellidos,
-          dni: matricula.student.dni,
-          direccion: matricula.student.direccion,
-          telefono: matricula.student.telefono,
-          email: matricula.student.email,
         },
         subject: {
           id: matricula.subject.id,
@@ -39,11 +40,60 @@ export class GetMatriculaController {
           id: matricula.teacher.id,
           nombre: matricula.teacher.nombre,
           apellidos: matricula.teacher.apellidos,
-          email: matricula.teacher.email,
           asignaturas: matricula.teacher.asignaturas,
         },
       };
       return response.status(200).json(matriculaDto);
+    } catch (error) {
+      console.error(error);
+      return response
+        .status(500)
+        .json({ message: 'Error interno del servidor' });
+    }
+  }
+
+  // OBTENER MATRICULA POR ID DEL STUDENT
+
+  @Get('student/:id')
+  async getMatriculasByStudentId(
+    @Param('id') id: number,
+    @Res() response: Response,
+  ) {
+    try {
+      const studentId = Number(id);
+      const matriculas =
+        await this.getMatriculaService.getMatriculasByStudentId(studentId);
+
+      if (!matriculas || matriculas.length === 0) {
+        return response
+          .status(404)
+          .json({ message: 'Matrículas no encontradas para este estudiante' });
+      }
+      // OPCION DE CONTROLAR LOS DATOS MEDIANTE UN DTO
+      const student = await this.getStudentService.getStudent(studentId);
+      if (!student || matriculas.length === 0) {
+        return response
+          .status(404)
+          .json({ message: 'No se encuentra este estudiante' });
+      }
+
+      const matriculasDto = {
+        student, // es necesario recibir el estudiante???
+        matriculas: matriculas.map(({ id, subject, teacher }) => ({
+          id,
+          subject: {
+            id: subject.id,
+            nombre: subject.nombre,
+          },
+          teacher: {
+            id: teacher.id,
+            nombre: teacher.nombre,
+            apellidos: teacher.apellidos,
+            asignaturas: teacher.asignaturas,
+          },
+        })),
+      };
+      return response.status(200).json(matriculasDto);
     } catch (error) {
       console.error(error);
       return response
--- app-gestion-server/src/Controllers/Matricula/Get/getMatricula.service.ts
+++ app-gestion-server/src/Controllers/Matricula/Get/getMatricula.service.ts
@@ -9,7 +9,10 @@ export class GetMatriculaService {
     @InjectRepository(MatriculaDb)
     private readonly matriculaRepository: Repository<MatriculaDb>,
   ) {}
+
+  // OBTENER MATRICULAS POR ID DE MATRICULA
   async getMatricula(id: number): Promise<MatriculaDb | null> {
+    // se pone null para gestionar mejor si no se obtiene ninguna matricula, porque si no devuelve undefined
     const matricula = await this.matriculaRepository
       .createQueryBuilder('matricula')
       .leftJoinAndSelect('matricula.student', 'student')
@@ -19,5 +22,17 @@ export class GetMatriculaService {
       .getOne();
 
     return matricula || null;
+  }
+
+  // OBTENER MATRICULAS POR ID DE STUDENT
+  async getMatriculasByStudentId(studentId: number): Promise<MatriculaDb[]> {
+    const matriculas = await this.matriculaRepository.find({
+      where: {
+        student: { id: studentId },
+      },
+      relations: ['student', 'subject', 'teacher'],
+    });
+
+    return matriculas || [];
   }
 }
