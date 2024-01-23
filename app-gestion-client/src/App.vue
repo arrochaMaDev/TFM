851e10fb888b8233f607e3143a0e3bac20b8ef37
arrochaMaDev <arrocha.ma.dev@gmail.com>
2024-01-23 16:26:06 +0000

    copia seguridad antes de la importación de Sakai vue template

--- app-gestion-client/src/App.vue
+++ app-gestion-client/src/App.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import { onMounted, ref, watch, type Ref, inject } from 'vue'
-import { getUserData } from './utils/getUserData'
 import router from './router'
 import Spinner from './components/Spinner.vue'
 import { useLoadingStore } from './stores/loading'
 import { useAdminStore } from './stores/isAdmin'
 import type { VueCookies } from 'vue-cookies'
+
+
 
 // FUNCION LOADING SPINNER
 const loadingStore = useLoadingStore() // store de Pinia para el Spinner
@@ -72,171 +73,7 @@ const deleteCookie = (user: string) => {
     <div class="wrapper">
       <HelloWorld msg="You did it!" /> -->
 
-  <div class="container-fluid">
-    <div class="row flex-nowrap">
-      <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0">
-        <div
-          class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100"
-        >
-          <a
-            href="/"
-            class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none"
-          >
-            <span class="fs-5 d-none d-sm-inline">Menu</span>
-          </a>
-          <ul
-            class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start"
-            id="menu"
-          >
-            <li class="nav-item">
-              <a href="#" class="nav-link align-middle px-0">
-                <i class="fs-4 bi-house"></i> <span class="ms-1 d-none d-sm-inline">Home</span>
-              </a>
-            </li>
-            <li>
-              <a href="#submenu1" data-bs-toggle="collapse" aria-expanded="true" class="nav-link px-0 align-middle"
-              >
-                <i class="fs-4 bi-speedometer2"></i>
-                <span class="ms-1 d-none d-sm-inline">Registro</span>
-              </a> 
-              <ul class="collapse show nav flex-column ms-1" id="submenu1" data-bs-parent="#menu">
-                <li class="w-100">
-                  <RouterLink to="/alta-usuario" v-if="adminMode" class="nav-link px-0">
-                    <span class="d-none d-sm-inline">Nuevo Usuario</span>
-                  </RouterLink>
-                </li>
-                <li class="w-100">
-                  <RouterLink to="/alta-alumno" v-if="adminMode" class="nav-link px-0">
-                    <span class="d-none d-sm-inline">Nuevo Alumno</span>
-                  </RouterLink>
-                </li>
-                <li class="w-100">
-                  <RouterLink to="/alta-profesor" v-if="adminMode" class="nav-link px-0">
-                    <span class="d-none d-sm-inline">Nuevo Profesor</span>
-                  </RouterLink>
-                </li>
-                <li class="w-100">
-                  <RouterLink to="/alta-asignatura" v-if="adminMode" class="nav-link px-0">
-                    <span class="d-none d-sm-inline">Nueva Asignatura</span>
-                  </RouterLink>
-                </li>
-                <li class="w-100">
-                  <RouterLink
-                    to="/alta-asignatura-profesor"
-                    v-if="adminMode"
-                    class="nav-link px-0"
-                  >
-                    <span class="d-none d-sm-inline">Nueva Asignación</span>
-                  </RouterLink>
-                </li>
-                <li class="w-100">
-                  <RouterLink
-                    to="/matricula"
-                    v-if="adminMode"
-                    class="nav-link px-0"
-                  >
-                    <span class="d-none d-sm-inline">Nueva Matrícula</span>
-                  </RouterLink>
-                </li>
-              </ul>
-            </li> 
-
-            <li>
-              <a href="#" class="nav-link px-0 align-middle">
-                <i class="fs-4 bi-table"></i> 
-                <span class="ms-1 d-none d-sm-inline">Orders</span>
-                </a
-              >
-            </li>
-            <li>
-              <a href="#submenu2" data-bs-toggle="collapse" class="nav-link px-0 align-middle">
-                <i class="fs-4 bi-bootstrap"></i>
-                <span class="ms-1 d-none d-sm-inline">Bootstrap</span></a
-              >
-              <ul class="collapse nav flex-column ms-1" id="submenu2" data-bs-parent="#menu">
-                <li class="w-100">
-                  <a href="#" class="nav-link px-0">
-                    <span class="d-none d-sm-inline">Item</span> 1</a
-                  >
-                </li>
-                <li>
-                  <a href="#" class="nav-link px-0">
-                    <span class="d-none d-sm-inline">Item</span> 2</a
-                  >
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a href="#submenu3" data-bs-toggle="collapse" class="nav-link px-0 align-middle">
-                <i class="fs-4 bi-grid"></i> <span class="ms-1 d-none d-sm-inline">Products</span>
-              </a>
-              <ul class="collapse nav flex-column ms-1" id="submenu3" data-bs-parent="#menu">
-                <li class="w-100">
-                  <a href="#" class="nav-link px-0">
-                    <span class="d-none d-sm-inline">Product</span> 1</a
-                  >
-                </li>
-                <li>
-                  <a href="#" class="nav-link px-0">
-                    <span class="d-none d-sm-inline">Product</span> 2</a
-                  >
-                </li>
-                <li>
-                  <a href="#" class="nav-link px-0">
-                    <span class="d-none d-sm-inline">Product</span> 3</a
-                  >
-                </li>
-                <li>
-                  <a href="#" class="nav-link px-0">
-                    <span class="d-none d-sm-inline">Product</span> 4</a
-                  >
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a href="#" class="nav-link px-0 align-middle">
-                <i class="fs-4 bi-people"></i>
-                <span class="ms-1 d-none d-sm-inline">Customers</span>
-              </a>
-            </li>
-          </ul>
-          <hr />
-          <div v-if="userEmail" class="dropdown pb-4">
-            <a
-              href="#"
-              class="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
-              id="dropdownUser1"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              <img
-                src="https://github.com/mdo.png"
-                alt="hugenerd"
-                width="30"
-                height="30"
-                class="rounded-circle"
-              />
-              <span class="d-none d-sm-inline mx-1">{{ userEmail }}</span>
-            </a>
-            <ul
-              class="dropdown-menu dropdown-menu-dark text-small shadow"
-              aria-labelledby="dropdownUser1"
-            >
-              <li><a class="dropdown-item" href="#">Profile</a></li>
-              <li><a class="dropdown-item" href="#">Settings</a></li>
-              <li>
-                <hr class="dropdown-divider" />
-              </li>
-              <li>
-                <a v-if="!userEmail" class="dropdown-item" @click="">LogIn</a>
-                <a class="dropdown-item" @click="cerrarSesion">LogOut</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div> 
+    
   <nav>
     <!-- <RouterLink to="/home">Home</RouterLink> -->
     <RouterLink to="/about">About</RouterLink>
@@ -270,10 +107,7 @@ const deleteCookie = (user: string) => {
 </template>
 
 <style scoped>
-[class='col-auto col-md-3 col-xl-2 px-sm-2 px-0'] {
-  background-color: grey;
-  width: 200px;
-}
+
 .nav-link span {
   color: black;
 }
@@ -310,8 +144,6 @@ nav a.router-link-exact-active:hover {
 nav a {
   display: flex;
   flex-direction: column;
-  /* adding: 0 1rem;
-  border-left: 1px solid var(--color-border);  */
   cursor: pointer;
 }
 
--- app-gestion-client/src/main.ts
+++ app-gestion-client/src/main.ts
@@ -4,28 +4,30 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import VueCookies from 'vue-cookies'
 
-import Vuesax from 'vuesax'
-
-import 'vuesax/dist/vuesax.css' //Vuesax styles
-
 import App from './App.vue'
 import router from './router'
 
-// import '@/assets/css/bootstrap.min.css'
-// import '@/assets/js/bootstrap.bundle.min.js'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap'
-import '../node_modules/bootstrap-icons/font/bootstrap-icons.css'
+// PRIMEVUE
+import PrimeVue from 'primevue/config';
+// import 'primevue/resources/themes/lara-light-amber/theme.css';
+// import 'primevue/resources/themes/lara-dark-amber/theme.css';
+
+// import '../node_modules/primeflex/primeflex.css'
+// import 'primeflex/primeflex.scss';
+// import 'primeicons/primeicons.css'
+
+// import '@/assets/styles.scss';
 
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-// app.use(Vuesax, {
-//   // Options here
-// })
 app.use(VueCookies, {
   expires: '1d'
 })
+app.use(PrimeVue)
+
+
+
 app.mount('#app')
